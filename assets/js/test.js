45c63e17ef5c185660fa7bb37d7a84f252057423
Misato Yamamoto <misa_to_1213@hotmail.com>
2021-09-28 23:03:39 +0900

    20210928

---
+++ assets/js/test.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('#form').submit(function (event) {
+      var formData = $('#form').serialize();
+      $.ajax({
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfVIuGg_H8wvlF_5tegNP6f3nrUG7FUY9IpQ5elYT-FKVVosA/formResponse",
+        data: formData,
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+          0: function () {
+            $(".end-message").slideDown();
+            $(".submit-btn").fadeOut();
+            //window.location.href = "thanks.html";
+          },
+          200: function () {
+            $(".false-message").slideDown();
+          }
+        }
+      });
+      event.preventDefault();
+    });
+
+  });
